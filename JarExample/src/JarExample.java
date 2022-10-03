83d837fd1a662fc04f604a33ea13c0cd9f99f6ca
Zafer ONAN <32078405+zaferonan@users.noreply.github.com>
2022-10-03 22:06:31 +0300

    Hesap Makinesi Library Jar Example

--- JarExample/src/JarExample.java
+++ JarExample/src/JarExample.java
@@ -5,10 +5,10 @@ public class JarExample {
 	public static void main(String[] args) {
 
 		HesapMakinesi hesapMakinesi = new HesapMakinesi();
-		System.out.println("5 x 5 = " + HesapMakinesi.çarp(5, 5));
-		System.out.println("45 + 5 = " + HesapMakinesi.ekle(45, 5));
-		System.out.println("25 - 8 = " + HesapMakinesi.cikar(25, 8));
-		System.out.println("15 / 5 = " + HesapMakinesi.bol(15, 5));
+		System.out.println("5 x 5 = " + hesapMakinesi.çarp(5, 5));
+		System.out.println("45 + 5 = " + hesapMakinesi.ekle(45, 5));
+		System.out.println("25 - 8 = " + hesapMakinesi.cikar(25, 8));
+		System.out.println("15 / 5 = " + hesapMakinesi.bol(15, 5));
 
 	}
 
